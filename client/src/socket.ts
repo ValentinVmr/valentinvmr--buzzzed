b0b5f5dc629dbf9d0ab9bb82cbb184a2a79eb06d
ValentinVmr <valentin.vamour@gmail.com>
2025-02-09 15:30:40 +0100

    fix: wrong host

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = "http://localhost:9876" //"https://api-buzzzed.sleeplessnight.fr/";
+const URL = process.env['ENV'] === 'local' ? "http://localhost:9876" : "https://api-buzzzed.sleeplessnight.fr/";
 
 export const socket = io(URL);
